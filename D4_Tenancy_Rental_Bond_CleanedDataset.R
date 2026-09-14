5764671c75fa2770c895d53f42b28ad0ccf85b67
PrasanthiS19 <prasanthi.sooriyakumar@outlook.com>
2026-09-14 15:12:39 +1200

    Minor changes to data cleaning R code and add CSV output
    Minor changes to data cleaning R code and add CSV output

--- D4_Tenancy_Rental_Bond_CleanedDataset.R
+++ D4_Tenancy_Rental_Bond_CleanedDataset.R
@@ -1,18 +1,23 @@
+library(dplyr)
+library(tidyr)
+
+
 ##Read Tenancy_Renta_Bond
 Tenancy_Rental_Bond_DATE_CLEANING <- read.csv("https://github.com/ChiduruppalaPooja/christchurch-rental-market-analysis/blob/staging/Tenancy_Rental_Bond_DATE_CLEANING.csv", header=TRUE, row.names = NULL)
+
 
 # Drop columns Upper_Quartile_Rent, Lower_Quartile_Rent and Log_Std_Dev_Weekly_Rent
 df_clean_column <- subset(Tenancy_Rental_Bond_DATE_CLEANING, select = -c(Upper_Quartile_Rent, Lower_Quartile_Rent, Log_Std_Dev_Weekly_Rent))
 
 
-library(dplyr)
-library(tidyr)
 
 clean_df_na_Tenancy <- df_clean_column %>%
   # Convert both literal "NULL" text and "-99" into proper NA values
   mutate(across(where(is.character), ~na_if(., "NULL"))) %>%
   mutate(across(where(is.character), ~na_if(., "-99")))
 
-#The R script has the following function to drop rows with na data commented in the script. We have decided not to remove any unavailable data at this stage. 
+write.csv(clean_df_na_Tenancy, "clean_df_na_Tenancy.csv")
+
+#The R script has the following function to drop rows with na data commented in the script. We have decided not to remove any unavailable data at this stage.
 # Drop the rows
 # drop_na(clean_df_na_Tenancy)
